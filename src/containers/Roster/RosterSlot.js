1b3885a0ae86f27be497923ba3c6c27c40243967
Evan Lane <evanlane@Evans-MacBook-Pro.local>
2022-01-25 17:04:37 -0500

    added dynamic roster slots

---
+++ src/containers/Roster/RosterSlot.js
@@ -0,0 +1,23 @@
+import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
+
+const Slot = styled.div`
+  margin-top: 25px;
+`
+const SelectButton = styled.button`
+background: ${props => props.selectedSlot && props.selectedSlot === props.name ? 'darkred' : 'limegreen'}
+`
+
+function RosterSlot({isLocked, isInactive, test, teamInfo, slotName, leagueName, selectedSlot}) {
+
+      return (
+          isInactive ? null : 
+          <Slot>
+            {slotName}: {teamInfo.city} {teamInfo.name}
+            {isLocked ? " - Locked" : <SelectButton selectedSlot={selectedSlot} name={leagueName} onClick={() => test(leagueName)}>Move</SelectButton>}
+        </Slot>
+    
+  )
+}
+
+export default RosterSlot
--- src/containers/Roster/index.js
+++ src/containers/Roster/index.js
@@ -1,60 +1,62 @@
-import React, {useState, useEffect} from 'react';
-import styled from 'styled-components';
-import useRoster from './useRoster'
-import {convertMonthToReadable} from '../../utils'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import useRoster from "./useRoster";
+import { convertMonthToReadable } from "../../utils";
+import RosterSlot from "./RosterSlot";
+import {useSelector} from 'react-redux'
 
-const Container =  styled.div``
+const Container = styled.div``;
 const Slot = styled.div`
   margin-top: 25px;
-`
+`;
 const SelectButton = styled.button`
-background: ${props => props.selectedSlot && props.selectedSlot === props.name ? 'darkred' : 'limegreen'}
-`
+  background: ${(props) =>
+    props.selectedSlot && props.selectedSlot === props.name
+      ? "darkred"
+      : "limegreen"};
+`;
 
-const BannerMessage =  styled.div`
-
-`
+const BannerMessage = styled.div``;
 
 const MonthButton = styled.button`
-background: ${props => props.selectedMonth ? 'limegreen' : 'white'}
-`
+  background: ${(props) => (props.selectedMonth ? "limegreen" : "white")};
+`;
 
 const ScrollMenuContainer = styled.div`
-    background-color: #333;
-    width: 450px;
-    display: flex;
-    flex-direction: row;
-`
+  background-color: #333;
+  width: 450px;
+  display: flex;
+  flex-direction: row;
+`;
 
 const ScrollMenu = styled.div`
-    overflow: auto;
-    white-space: nowrap;
-    display: inline-block;
-    left: 100px;
-`
+  overflow: auto;
+  white-space: nowrap;
+  display: inline-block;
+  left: 100px;
+`;
 
 const ScrollMenuLink = styled.a`
-    display: inline-block;
-    color: ${props => props.selected ? 'limegreen' : 'white'};
-    text-align: center;
-    padding: 14px;
-    text-decoration: none;
-    width: 100px;
-    cursor: pointer;
-`
+  display: inline-block;
+  color: ${(props) => (props.selected ? "limegreen" : "white")};
+  text-align: center;
+  padding: 14px;
+  text-decoration: none;
+  width: 100px;
+  cursor: pointer;
+`;
 
 const ScrollMenuButton = styled.a`
-    display: inline-block;
-    color: white;
-    text-align: center;
-    padding: 14px;
-    text-decoration: none;
-    width: 25px;
-    cursor: pointer;
-`
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 14px;
+  text-decoration: none;
+  width: 25px;
+  cursor: pointer;
+`;
 
 function Roster(props) {
-  
   const {
     selectedRoflYear,
     roflMonth,
@@ -65,106 +67,175 @@ function Roster(props) {
     areRostersEqual,
     setRoflMonth,
     handleSubmit,
-    activeYears,
     activeRoflMonths
-} = useRoster()
+  } = useRoster();
 
-console.log('active rofl months')
-console.log(activeRoflMonths)
+  const {activeYears} = useSelector(state => ({
+    ...state.sportReducer
+  }))
 
-const [appliedScroll, setAppliedScroll] = useState(false)
+  const isActiveTable = {}
 
-const currentMonthRoster = roster ? roster[`${roflMonth}-${selectedRoflYear}`] : null
-const scrollLeft = () => {
-    scrollRef.current.scrollLeft -= 400
-}
+    activeRoflMonths && selectedRoflYear && activeRoflMonths[selectedRoflYear].forEach(month => {
+        isActiveTable[month.leagueId] = month.roflMonth
+    })
 
-const scrollRight = () => {
-    scrollRef.current.scrollLeft += 400
-}
+  const [appliedScroll, setAppliedScroll] = useState(false);
 
-const scrollRef = React.createRef()
+  const currentMonthRoster = roster
+    ? roster[`${roflMonth}-${selectedRoflYear}`]
+    : null;
+  const scrollLeft = () => {
+    scrollRef.current.scrollLeft -= 400;
+  };
 
-useEffect(() => {
-    if(scrollRef && scrollRef.current && !appliedScroll){
-        scrollRef.current.scrollLeft += ((roflMonth - 1) * 107)
-        setAppliedScroll(true)
+  const scrollRight = () => {
+    scrollRef.current.scrollLeft += 400;
+  };
+
+  const scrollRef = React.createRef();
+
+  useEffect(() => {
+    if (scrollRef && scrollRef.current && !appliedScroll) {
+      scrollRef.current.scrollLeft += (roflMonth - 1) * 107;
+      setAppliedScroll(true);
     }
-}, [scrollRef]);
+  }, [scrollRef]);
 
-const monthsForScroll = (year) => {
-    year = Number(year)
+  const monthsForScroll = (year) => {
+    year = Number(year);
     return [
-        {number: 1, month: `April ${year}`},
-        {number: 2, month: `May ${year}`},
-        {number: 3, month: `June ${year}`},
-        {number: 4, month: `July ${year}`},
-        {number: 5, month: `August ${year}`},
-        {number: 6, month: `September ${year}`},
-        {number: 7, month: `October ${year}`},
-        {number: 8, month: `November ${year}`},
-        {number: 9, month: `December ${year}`},
-        {number: 10, month: `January ${year+1}`},
-        {number: 11, month: `February ${year+1}`},
-        {number: 12, month: `March ${year+1}`},
-        {number: 13, month: `April ${year+1}`},
-        {number: 14, month: `May ${year+1}`}
-    ]
-}
+      { number: 1, month: `April ${year}` },
+      { number: 2, month: `May ${year}` },
+      { number: 3, month: `June ${year}` },
+      { number: 4, month: `July ${year}` },
+      { number: 5, month: `August ${year}` },
+      { number: 6, month: `September ${year}` },
+      { number: 7, month: `October ${year}` },
+      { number: 8, month: `November ${year}` },
+      { number: 9, month: `December ${year}` },
+      { number: 10, month: `January ${year + 1}` },
+      { number: 11, month: `February ${year + 1}` },
+      { number: 12, month: `March ${year + 1}` },
+      { number: 13, month: `April ${year + 1}` },
+      { number: 14, month: `May ${year + 1}` }
+    ];
+  };
+
+  const SlotContent = (leagueInfo, leagueName) => {
+    <Slot>
+      {leagueInfo.name}: {leagueInfo.city} {leagueInfo.name}{" "}
+      <SelectButton
+        selectedSlot={selectedSlot}
+        name={leagueName}
+        onClick={() => test(leagueName)}
+      >
+        Move
+      </SelectButton>
+    </Slot>;
+  };
 
   return (
     <Container>
-      
-        <p>Welcome to roster</p>
-        
-        {
-          roster && currentOrganization && selectedRoflYear ?
-          <div>
-        <p>year: {selectedRoflYear}</p>
-            
-            <ScrollMenuContainer>
-            <ScrollMenuButton onClick={scrollLeft}>{'<'}</ScrollMenuButton>
+      <p>Welcome to roster</p>
+
+      {roster && currentOrganization && selectedRoflYear ? (
+        <div>
+          <p>year: {selectedRoflYear}</p>
+
+          <ScrollMenuContainer>
+            <ScrollMenuButton onClick={scrollLeft}>{"<"}</ScrollMenuButton>
             <ScrollMenu ref={scrollRef}>
-                {monthsForScroll(selectedRoflYear).map(item => (
-                    <ScrollMenuLink selected={item.number === roflMonth} key={item.number} onClick={() => setRoflMonth(item.number)}>{item.month}</ScrollMenuLink>
-                ))}
+              {monthsForScroll(selectedRoflYear).map((item) => (
+                <ScrollMenuLink
+                  selected={item.number === roflMonth}
+                  key={item.number}
+                  onClick={() => setRoflMonth(item.number)}
+                >
+                  {item.month}
+                </ScrollMenuLink>
+              ))}
             </ScrollMenu>
-            <ScrollMenuButton onClick={scrollRight}>{'>'}</ScrollMenuButton>
-            </ScrollMenuContainer>
-            
-        <p>month: {roflMonth}</p>
-        <Slot>{currentOrganization.league_1.name}: {currentMonthRoster.league_1.city} {currentMonthRoster.league_1.name} <SelectButton selectedSlot={selectedSlot} name={'league_1'} onClick={() => test('league_1')}>Move</SelectButton></Slot>
-        <Slot>{currentOrganization.league_2.name}: {currentMonthRoster.league_2.city} {currentMonthRoster.league_2.name}<SelectButton selectedSlot={selectedSlot} name={'league_2'} onClick={() => test('league_2')}>Move</SelectButton></Slot>
-        {currentOrganization.league_3 ? 
-        <Slot>{currentOrganization.league_3.name}: {currentMonthRoster.league_3.city} {currentMonthRoster.league_3.name}<SelectButton selectedSlot={selectedSlot} name={'league_3'} onClick={() => test('league_3')}>Move</SelectButton></Slot>
-      : null}
-      {currentOrganization.league_4 ? 
-        <Slot>{currentOrganization.league_4.name}: {currentMonthRoster.league_4.city} {currentMonthRoster.league_4.name}<SelectButton selectedSlot={selectedSlot} name={'league_4'} onClick={() => test('league_4')}>Move</SelectButton></Slot>
-      : null}
-      {
-        Array.from(Array(currentOrganization.flex_spots)).map((x, i) => (
-          <Slot key={`p-${i}`}>FLEX: {currentMonthRoster[`flex_${i+1}`].city} {currentMonthRoster[`flex_${i+1}`].name}<SelectButton selectedSlot={selectedSlot} name={`flex_${i+1}`} onClick={() => test(`flex_${i+1}`)}>Move</SelectButton></Slot>
-        ))
-      }
-      {
-        Array.from(Array(currentOrganization.bench_spots)).map((x, i) => (
-          <Slot key={`p-${i}`}>BENCH: {currentMonthRoster[`bench_${i+1}`].city} {currentMonthRoster[`bench_${i+1}`].name}<SelectButton selectedSlot={selectedSlot} name={`bench_${i+1}`} onClick={() => test(`bench_${i+1}`)}>Move</SelectButton></Slot>
-        ))
-      }
-      {
-         areRostersEqual ? null :
-           <BannerMessage>
-             You have unsaved changes
+            <ScrollMenuButton onClick={scrollRight}>{">"}</ScrollMenuButton>
+          </ScrollMenuContainer>
+
+          <p>month: {roflMonth}</p>
+          <RosterSlot
+            isLocked={roflMonth <= isActiveTable[currentMonthRoster.league_1.sport_league.id]}
+            isInactive={0 === currentMonthRoster.league_1.sport_league.active}
+            test={test}
+            teamInfo={currentMonthRoster.league_1}
+            selectedSlot={selectedSlot}
+            slotName={currentOrganization.league_1.name}
+            leagueName="league_1"
+          />
+          <RosterSlot
+                      isLocked={roflMonth <= isActiveTable[currentMonthRoster.league_2.sport_league.id]}
+                      isInactive={0 === currentMonthRoster.league_2.sport_league.active}
+            table={isActiveTable}
+            test={test}
+            teamInfo={currentMonthRoster.league_2}
+            selectedSlot={selectedSlot}
+            slotName={currentOrganization.league_2.name}
+            leagueName="league_2"
+          />
+          {currentOrganization.league_3 ? (
+            <RosterSlot
+            isLocked={roflMonth <= isActiveTable[currentMonthRoster.league_3.sport_league.id]}
+            isInactive={0 === currentMonthRoster.league_3.sport_league.active}
+              table={isActiveTable}
+              test={test}
+              teamInfo={currentMonthRoster.league_3}
+              selectedSlot={selectedSlot}
+              slotName={currentOrganization.league_3.name}
+              leagueName="league_3"
+            />
+          ) : null}
+          {currentOrganization.league_4 ? (
+            <RosterSlot
+            isLocked={roflMonth <= isActiveTable[currentMonthRoster.league_4.sport_league.id]}
+            isInactive={0 === currentMonthRoster.league_4.sport_league.active}
+              test={test}
+              teamInfo={currentMonthRoster.league_4}
+              selectedSlot={selectedSlot}
+              slotName={currentOrganization.league_4.name}
+              leagueName="league_4"
+            />
+          ) : null}
+          {Array.from(Array(currentOrganization.flex_spots)).map((x, i) => (
+            <RosterSlot
+            isLocked={roflMonth <= isActiveTable[currentMonthRoster[`flex_${i + 1}`].sport_league.id]}
+            isInactive={0 === currentMonthRoster[`flex_${i + 1}`].sport_league.active}
+              test={test}
+              teamInfo={currentMonthRoster[`flex_${i + 1}`]}
+              selectedSlot={selectedSlot}
+              slotName={`FLEX`}
+              leagueName={`flex_${i + 1}`}
+            />
+          ))}
+          {Array.from(Array(currentOrganization.bench_spots)).map((x, i) => (
+            <RosterSlot
+            isLocked={roflMonth <= isActiveTable[currentMonthRoster[`bench_${i + 1}`].sport_league.id]}
+            isInactive={0 === currentMonthRoster[`bench_${i + 1}`].sport_league.active}
+              test={test}
+              teamInfo={currentMonthRoster[`bench_${i + 1}`]}
+              selectedSlot={selectedSlot}
+              slotName={`BENCH`}
+              leagueName={`bench_${i + 1}`}
+            />
+          ))}
+          {areRostersEqual ? null : (
+            <BannerMessage>
+              You have unsaved changes
               <button onClick={handleSubmit}>Submit</button>
-           </BannerMessage>
-      }
-          </div>
-          :
-          <p>loading...</p>
-        }
-        
+            </BannerMessage>
+          )}
+        </div>
+      ) : (
+        <p>loading...</p>
+      )}
     </Container>
-  )
+  );
 }
 
-export default Roster
+export default Roster;
