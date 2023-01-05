223f93ccf93f1d130df5d2e10b1068e6ce3dca0a
Evan Lane <evanlane@Evans-MacBook-Pro.local>
2023-01-05 01:14:04 -0500

    reverted type for bid deletion

--- src/components/Modals/BidDetails.js
+++ src/components/Modals/BidDetails.js
@@ -87,7 +87,7 @@ function BidDetails() {
         try {
           var res = await makeRequest({
             method: "delete",
-            route: `users/bids/delete/${Number(bidId)}`
+            route: `users/bids/delete/${bidId}`
           });
           const body = res.body;
           //   if success show message
