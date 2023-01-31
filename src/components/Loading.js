d8b07d4d4fe419dce748563f006a0a73ee625c5a
Evan Lane <evanlane@Evans-MacBook-Pro.local>
2023-01-31 13:09:02 -0500

    loadinig spinner test

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import styled, {keyframes} from 'styled-components'
+import { blue } from '../constants/style';
+
+const spinner = keyframes`
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+`
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 50px;
+`
+const Loader = styled.div`
+    border: 16px solid #f3f3f3;
+    border-top: 16px solid ${blue}; 
+    border-radius: 50%;
+    width: 300px;
+    height: 300px;
+    animation: ${spinner} 2s linear infinite;
+    position: fixed;
+`
+
 
 function Loading() {
     return(
-    <div style={{marginTop: '50px'}}>
-        <p>Loading......</p>
-    </div>
+        <Container>
+        <Loader></Loader>
+        <img src="https://rofl-public-assets.s3.us-east-2.amazonaws.com/MSFALogo.PNG" height="300" width="300"/>
+        </Container>
     )
 }
 
