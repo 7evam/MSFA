2ea362b47370587eeb384ac12bae25c8a4fe6c25
Evan Lane <evanlane@MacBook-Pro.local>
2023-04-21 00:05:57 -0400

    refactored transactions to separate concerns

--- src/containers/Scoring/index.js
+++ src/containers/Scoring/index.js
@@ -15,176 +15,24 @@ import {
   ScItem,
   League,
 } from './components';
+import useScoring from './useScoring';
 
 function Scoring() {
-  const { currentOrganization } = useSelector((state) => ({
-    ...state.authReducer,
-  }));
-
   const {
-    activeYears,
-    startingMonths,
     playoffMonths,
-    sportTeams,
     selectedYear,
-  } = useSelector((state) => ({
-    ...state.sportReducer,
-  }));
-
-  const { makeRequest } = useApi();
-
-  const { hydrateSportTeams, hydrateActiveYears } = useHydration();
-
-  const [scores, setScores] = useState(null);
-  const [roflMonth, setRoflMonth] = useState(null);
-  const [league, setLeague] = useState(null);
-  const [finalMonthForDisplay, setFinalMonthForDisplay] = useState(null);
-  const [display, setDisplay] = useState('score');
-  const [firstMonthForDisplay, setFirstMonthForDisplay] = useState(null);
-  const [readyToRender, setReadyToRender] = useState(false);
-  // const [renderCount, setRenderCount] = useState(0);
-  const [error, setError] = useState(null);
-
-  // find the current, first and last rofl month for display
-  const setDisplayMonthRange = (selectedLeague) => {
-    const newFinalMonth = activeYears[selectedYear][selectedLeague]
-      ? activeYears[selectedYear][selectedLeague].roflMonth
-      : playoffMonths[selectedYear][selectedLeague];
-    const newStartingMonth = startingMonths[selectedYear][selectedLeague];
-    const newCurrentMonth = roflMonth > newStartingMonth && roflMonth < newFinalMonth
-      ? roflMonth
-      : newFinalMonth;
-    setFinalMonthForDisplay(newFinalMonth);
-    setFirstMonthForDisplay(newStartingMonth);
-    setRoflMonth(newCurrentMonth);
-  };
-
-  const calculateMonthsToDisplay = (records) => {
-    const allMonths = [];
-    Object.keys(records).forEach((leagueId) => {
-      Object.keys(records[leagueId]).forEach((monthKey) => {
-        allMonths.push(Number(monthKey.split('-')[0]));
-      });
-    });
-    setFinalMonthForDisplay(Math.max(...allMonths));
-    // PROBLEM on re-render 2023 is defined in startingMonths
-    setFirstMonthForDisplay(startingMonths[selectedYear][league]);
-  };
-
-  const fetchScores = async (abortController) => {
-    const res = await makeRequest({
-      method: 'get',
-      route: `/sports/scores/${currentOrganization.id}/${selectedYear}`,
-      abort: abortController,
-    });
-    if (res.statusCode === 200) {
-      const scores = res.body;
-      calculateMonthsToDisplay(scores.records);
-      setScores(scores);
-    }
-  };
-
-  // --use effects for initial render--
-
-  // this function runs on page load and makes all api requests
-  useEffect(() => {
-    async function fetchData() {
-      const abortController = new AbortController();
-      //   firstMonthForDisplay
-      //   finalMonthForDisplay
-      //   league
-      //   scores
-      //   sportTeams
-      try {
-        await fetchScores(abortController);
-        if (!sportTeams) await hydrateSportTeams(abortController);
-        if (!activeYears) await hydrateActiveYears(abortController);
-        const activeLeagueArray = Object.keys(activeYears[selectedYear]);
-        const startingActiveLeague = Math.min(...activeLeagueArray);
-        setLeague(startingActiveLeague);
-        setDisplayMonthRange(startingActiveLeague);
-        setReadyToRender(true);
-      } catch (e) {
-        setError(error);
-      }
-      return () => abortController.abort();
-    }
-    fetchData();
-  }, []);
-
-  // when all api requests have been made, update state derrived from api calls
-  //   useEffect(() => {
-  //     if (scores && activeYears && !readyToRender) {
-  //       const activeLeagueArray = Object.keys(activeYears[selectedYear]);
-  //       const startingActiveLeague = Math.min(...activeLeagueArray);
-  //       setLeague(startingActiveLeague);
-  //       setDisplayMonthRange(startingActiveLeague);
-  //     }
-  //   }, [scores, league, finalMonthForDisplay, firstMonthForDisplay]);
-
-  // if all derrived state has been set, the app must be ready to render
-  // useEffect(() => {
-  //   if (
-  //     firstMonthForDisplay
-  //     && finalMonthForDisplay
-  //     && league
-  //     && scores
-  //     && sportTeams
-  //     && !readyToRender
-  //     && !reLoading
-  //   ) {
-  //     setReadyToRender(true);
-  //   }
-  // }, [scores, league, finalMonthForDisplay, firstMonthForDisplay, sportTeams, reLoading]);
-
-  // initial requests functiioin
-  //   const makeInitialRequests = async (abortController) => {
-  //     console.log('stp 1');
-
-  //     console.log('step 2');
-  //   };
-
-  //  --post initial render use effect--
-
-  //  change data on league change
-  useEffect(() => {
-    async function reCalculate() {
-      setReadyToRender(false);
-      await setDisplayMonthRange(league);
-      setReadyToRender(true);
-    }
-    if (readyToRender) reCalculate();
-  }, [league]);
-
-  //  change data on year change
-  useEffect(() => {
-    async function refetchData() {
-      setReadyToRender(false);
-      const abortController = new AbortController();
-      try {
-        console.log('before refetch scores');
-        await fetchScores(abortController);
-        console.log('after refetch scorees');
-        const activeLeagueArray = Object.keys(activeYears[selectedYear]);
-        const startingActiveLeague = Math.min(...activeLeagueArray);
-        setLeague(startingActiveLeague);
-        setDisplayMonthRange(startingActiveLeague);
-        setReadyToRender(true);
-      } catch (e) {
-        setError(error);
-      }
-      setReadyToRender(true);
-      return () => abortController.abort();
-    }
-    if (readyToRender) refetchData();
-  }, [selectedYear]);
-
-  // useEffect(() => {
-  //   setReadyToRender(false);
-  //   if (selectedYear) {
-  //     makeInitialRequests();
-  //   }
-  // }, [selectedYear]);
+    league,
+    roflMonth,
+    sportTeams,
+    scores,
+    firstMonthForDisplay,
+    finalMonthForDisplay,
+    setRoflMonth,
+    display,
+    setDisplay,
+    readyToRender,
+    setLeague,
+  } = useScoring();
 
   const renderSwitch = (componentToRender) => {
     switch (componentToRender) {
---
+++ src/containers/Scoring/useScoring.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import useApi from '../../hooks/useApi';
+import useHydration from '../../hooks/useHydration';
+
+function useScoring() {
+  const { currentOrganization } = useSelector((state) => ({
+    ...state.authReducer,
+  }));
+
+  const {
+    activeYears,
+    startingMonths,
+    playoffMonths,
+    sportTeams,
+    selectedYear,
+  } = useSelector((state) => ({
+    ...state.sportReducer,
+  }));
+
+  const { makeRequest } = useApi();
+
+  const { hydrateSportTeams, hydrateActiveYears } = useHydration();
+
+  const [scores, setScores] = useState(null);
+  const [roflMonth, setRoflMonth] = useState(null);
+  const [league, setLeague] = useState(null);
+  const [finalMonthForDisplay, setFinalMonthForDisplay] = useState(null);
+  const [display, setDisplay] = useState('score');
+  const [firstMonthForDisplay, setFirstMonthForDisplay] = useState(null);
+  const [readyToRender, setReadyToRender] = useState(false);
+  const [error, setError] = useState(null);
+
+  // find the current, first and last rofl month for display
+  const setDisplayMonthRange = (selectedLeague) => {
+    const newFinalMonth = activeYears[selectedYear][selectedLeague]
+      ? activeYears[selectedYear][selectedLeague].roflMonth
+      : playoffMonths[selectedYear][selectedLeague];
+    const newStartingMonth = startingMonths[selectedYear][selectedLeague];
+    const newCurrentMonth = roflMonth > newStartingMonth && roflMonth < newFinalMonth
+      ? roflMonth
+      : newFinalMonth;
+    setFinalMonthForDisplay(newFinalMonth);
+    setFirstMonthForDisplay(newStartingMonth);
+    setRoflMonth(newCurrentMonth);
+  };
+
+  const calculateMonthsToDisplay = (records) => {
+    const allMonths = [];
+    Object.keys(records).forEach((leagueId) => {
+      Object.keys(records[leagueId]).forEach((monthKey) => {
+        allMonths.push(Number(monthKey.split('-')[0]));
+      });
+    });
+    setFinalMonthForDisplay(Math.max(...allMonths));
+    setFirstMonthForDisplay(startingMonths[selectedYear][league]);
+  };
+
+  const fetchScores = async (abortController) => {
+    const res = await makeRequest({
+      method: 'get',
+      route: `/sports/scores/${currentOrganization.id}/${selectedYear}`,
+      abort: abortController,
+    });
+    if (res.statusCode === 200) {
+      const scores = res.body;
+      calculateMonthsToDisplay(scores.records);
+      setScores(scores);
+    }
+  };
+
+  // this function runs on page load and makes all api requests
+  useEffect(() => {
+    async function fetchData() {
+      const abortController = new AbortController();
+      try {
+        await fetchScores(abortController);
+        if (!sportTeams) await hydrateSportTeams(abortController);
+        if (!activeYears) await hydrateActiveYears(abortController);
+        const activeLeagueArray = Object.keys(activeYears[selectedYear]);
+        const startingActiveLeague = Math.min(...activeLeagueArray);
+        setLeague(startingActiveLeague);
+        setDisplayMonthRange(startingActiveLeague);
+        setReadyToRender(true);
+      } catch (e) {
+        setError(error);
+      }
+      return () => abortController.abort();
+    }
+    fetchData();
+  }, []);
+
+  //  change data on league change
+  useEffect(() => {
+    async function reCalculate() {
+      setReadyToRender(false);
+      await setDisplayMonthRange(league);
+      setReadyToRender(true);
+    }
+    if (readyToRender) reCalculate();
+  }, [league]);
+
+  //  change data on year change
+  useEffect(() => {
+    async function refetchData() {
+      setReadyToRender(false);
+      const abortController = new AbortController();
+      try {
+        console.log('before refetch scores');
+        await fetchScores(abortController);
+        console.log('after refetch scorees');
+        const activeLeagueArray = Object.keys(activeYears[selectedYear]);
+        const startingActiveLeague = Math.min(...activeLeagueArray);
+        setLeague(startingActiveLeague);
+        setDisplayMonthRange(startingActiveLeague);
+        setReadyToRender(true);
+      } catch (e) {
+        setError(error);
+      }
+      setReadyToRender(true);
+      return () => abortController.abort();
+    }
+    if (readyToRender) refetchData();
+  }, [selectedYear]);
+
+  return {
+    playoffMonths,
+    selectedYear,
+    league,
+    roflMonth,
+    sportTeams,
+    scores,
+    firstMonthForDisplay,
+    finalMonthForDisplay,
+    setRoflMonth,
+    display,
+    setDisplay,
+    readyToRender,
+    setLeague,
+  };
+}
+
+export default useScoring;
