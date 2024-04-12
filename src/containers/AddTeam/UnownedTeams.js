dac387a31ddc329784395c77ac02cbf730a556a1
Evan Lane <evan@Evans-MacBook-Pro.local>
2024-04-12 12:15:23 -0400

    updated unowned teams

--- src/containers/AddTeam/UnownedTeams.js
+++ src/containers/AddTeam/UnownedTeams.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import '@fontsource/open-sans';
@@ -12,7 +12,6 @@ import {
   Select,
   Label,
   Headline,
-  ActionButton,
   Container,
   League,
   slotData,
@@ -45,6 +44,103 @@ const TopText = styled.p`
   width: 85%;
   font-size: px;
   text-align: center;
+`;
+
+const ScoringContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
+    width: 100%;
+    margin-top: 15px;
+    border: 2px solid #E5EAF4;
+    width: 90%;
+    border-radius: 10px;
+    & div:nth-child(4n+5),
+    & div:nth-child(4n+6) {
+      background-color: #F7FBFF;
+    }
+  }
+`;
+
+
+
+const ActionButton = styled.button`
+    height: 120%;
+    width: 80%;
+    z-index: 100;
+    display: inline-block;
+    outline: none;
+    cursor: pointer;
+    font-weight: 500;
+    padding: 0 16px;
+    margin-left: 8px;
+    border-radius: 4px;
+    color: white;
+    background: ${(props) => (props.selectedSlot && props.selectedSlot === props.leagueKey
+    ? '#4E871F'
+    : '#17288F')};
+    line-height: 1.15;
+    font-size: 14px;
+    letter-spacing: .08em;
+    text-decoration: none;
+    text-transform: uppercase;
+    border: none;
+    text-align: center;
+    box-shadow: 0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%);
+    transition: box-shadow 280ms cubic-bezier(0.4, 0, 0.2, 1);
+    :hover {
+        background: #4E871F;
+        box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
+    }
+    @media (max-width: ${mobileBreakPoint}){
+        width: 80px;
+        height: 20px;
+       }
+`;
+
+const Cell = styled.div`
+    padding: 16px;
+    padding-right: 0px;
+    border-bottom: ${(props) => (props.isLastInList ? null : '2px solid #E5EAF4')}; 
+    background-color: ${(props) => (props.colored ? '#F7FBFF' : 'white')};
+    &:hover{
+        text-decoration: ${(props) => (props.teamName ? 'underline' : null)}; 
+        cursor: ${(props) => (props.teamName ? 'pointer' : null)}; 
+    }
+    @media (min-width: ${mobileBreakPoint}){
+        ${(props) => !props.firstItem && `
+            &:before {
+                content: "";
+                right: 0;
+                z-index: 100;
+                top: 0;
+                height: 50%; 
+                border-right: 2px solid #E5EAF4;
+                margin-right: 8px;
+            }
+        `}
+    }
+    
+    @media (max-width: ${mobileBreakPoint}){
+        font-size: 14px;
+    }
+`;
+
+const HeaderLabel = styled.div`
+    padding: 16px 0px 8px 16px;
+    text-align:center;
+    background-color: #F7FBFF;
+    font-weight: 800;
+    font-size: 14px;
+    &:hover{
+      cursor: pointer;
+      text-decoration: underline;
+  }
+    @media (max-width: ${mobileBreakPoint}){
+      font-size: 10px;
+      padding-right: 16px;
+     }
 `;
 
 function UnownedTeams({
@@ -140,7 +236,7 @@ function UnownedTeams({
           <p>
             Waiver period
             {' '}
-            { waiverPeriodEnded ? 'ended' : 'ends'}
+            {waiverPeriodEnded ? 'ended' : 'ends'}
             {' '}
             {convertDateObjToReadable(deadline)}
             {' '}
@@ -155,28 +251,28 @@ function UnownedTeams({
           </TopText>
           <TopText>
             {
-            waiverExceptions && waiverExceptions.length
-              ? 'Any teams that were dropped during the recently ended wavier period are now available to claim on waivers. You may press "claim" to place a bid for these teams'
-              : null
+              waiverExceptions && waiverExceptions.length
+                ? 'Any teams that were dropped during the recently ended wavier period are now available to claim on waivers. You may press "claim" to place a bid for these teams'
+                : null
             }
           </TopText>
-          <TitleRow>
-            <Th style={{ width: '200px' }}>Team</Th>
-            <Th style={{ width: '70px' }}>Action</Th>
-          </TitleRow>
-          {
+          <ScoringContainer>
+            <HeaderLabel>Team</HeaderLabel>
+            <HeaderLabel>Action</HeaderLabel>
+            {
 
-      unownedTeams[league].map((team) => (
-        <SlotRow key={team}>
-          <Td style={{ width: '200px' }}>
-            {sportTeams[league][team].city}
-            {' '}
-            {sportTeams[league][team].name}
-          </Td>
-          <Td style={{ width: '70px' }}>{getUnownedActionButton(league, team)}</Td>
-        </SlotRow>
-      ))
-}
+              unownedTeams[league].map((team) => (
+                <Fragment>
+                  <Cell firstItem>
+                    {sportTeams[league][team].city}
+                    {' '}
+                    {sportTeams[league][team].name}
+                  </Cell>
+                  <Cell>{getUnownedActionButton(league, team)}</Cell>
+                </Fragment>
+              ))
+            }
+          </ScoringContainer>
         </Container>
       ) : <Loading />
   );
